ab6c37ccb1f187ef6034de03eaf0cfee67104106
HarrisonLavin <harrison.lavin@gmail.com>
2016-04-01 10:50:51 -0400

    ...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,19 +26,15 @@ $(function(){ // on document ready,
     state_name = $('#state_name').val() // we get the artist name
     $('#state_name').val(''); 
     $.ajax({
-      method: "POST",
-      url: "https://api.instagram.com/v1/tags/"+ state_name +"/media/recent?access_token=ACCESS_TOKEN"
-,
-
+      type: "GET",
+      dataType: "jsonp",
+      cache: false,
+      url: "https://api.instagram.com/v1/tags/"+ state_name +"/media/recent?access_token=370648620.4f46f98.9ed4716de0184a9299b595e52ba248c0"
     }).done(function(data){
-      data.artists.items[0].images.forEach(function(element){
-        var url = element.url;
-        $('.images').append('<img src="' +  url + '" >');
-       
+     console.log(data)
       })
     })
   })
-})
 
 // https://api.spotify.com/v1/search?query=tania&type=artist
 // https://www.reddit.com/r/funny.json
